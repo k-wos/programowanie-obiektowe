b4c9d9c167f25d89802589a8dc2fbe5286f88a79
Kacper Wos <kacper.wos@microsoft.wsei.edu.pl>
2022-03-02 11:32:35 +0100

    1 lekcja

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -9,4 +9,42 @@ namespace lab_1
             Console.WriteLine("Hello World!");
         }
     }
+    public class PersonProperties
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    firstName = value;
+                }
+            }
+        }
+    }
+
+    public enum Currency
+    {
+        PLN = 1,
+        USD = 2,
+        EUR = 3
+    }
+
+    public class Money 
+    {
+        private readonly decimal _value;
+        private readonly Currency _currency;
+        private Money(decimal value, Currency currency)
+        {
+            _value = value;
+            _currency = currency;
+        }
+ 
+}
+
 }
